309a3120bf33052159e818a1b584863eae1a8b90
someone5678 <nemui3353@gmail.com>
2023-03-10 19:12:51 +0900

    mondrian: Track platform/external/gflags using vendorsetup.sh
    * Required dependency for snapuserd

---
+++ vendorsetup.sh
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 The TWRP Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+if [ ! -d .repo/local_manifests ];
+then
+    mkdir -p .repo/local_manifests
+fi
+
+if [ ! -f .repo/local_manifests/local_mondrian.xml ];
+then
+    cp device/xiaomi/mondrian/local_mondrian.xml .repo/local_manifests/local_mondrian.xml
+fi
+
+if [ ! -d external/gflags ];
+then
+    repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all) external/gflags
+fi
+
+# For building with minimal manifest
+export ALLOW_MISSING_DEPENDENCIES=true
